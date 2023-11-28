1b79b2b8c39d7e083748ef27db73961a820f9b9b
theo-baccam <theo.baccam@laplateforme.io>
2023-11-28 13:52:23 +0100

    mode 24h et 12h

--- horloge.py
+++ horloge.py
@@ -1,6 +1,17 @@
 import time
 
 
+def format_choix():
+    choix = input("Voulez vous afficher en:\n1) Mode 24 Heures\n2) Mode 12 Heures\n")
+    if choix == "1":
+        check = False
+    elif choix == "2":
+        check = True
+    else:
+        raise ValueError("Choix Invalide")
+    return check
+
+
 def afficher_heure(input_tuple):
     hh = input_tuple[0]
     mm = input_tuple[1]
@@ -13,42 +24,59 @@ def afficher_heure(input_tuple):
         raise ValueError("Secondes Invalide")
 
     while True:
+        if hh >= 12 and twelve == True:
+            tw = hh - 12
+            meridiem = "PM"
+        elif hh < 12 and twelve == True:
+            tw = hh
+            meridiem = "AM"
+
         ss = (ss + 60 + 1) % 60
+
         if ss == 0:
             mm = (mm + 60 + 1) % 60
-        if mm == 0:
-            hh = (hh + 24 + 1) % 24
 
-        current_time = f"{hh:02d}:{mm:02d}:{ss:02d}"
+        if mm == 0 and twelve == False:
+            hh = (hh + 24 + 1) % 24
+        elif mm == 0 and twelve == True:
+            tw = (tw + 12 + 1) % 12
+
+        if twelve == True:
+            current_time = f"{tw:02d}:{mm:02d}:{ss:02d} {meridiem}"
+        else: 
+            current_time = f"{hh:02d}:{mm:02d}:{ss:02d}"
+
         yield current_time
 
 
-# def alarme(input_tuple, input_hour):
-#     hh = input_tuple[0]
-#     mm = input_tuple[1]
-#     ss = input_tuple[2]
-#     if hh < 0 or hh > 24:
-#         raise ValueError("Heure Invalide")
-#     if mm < 0 or mm > 60:
-#         raise ValueError("Minutes Invalide")
-#     if ss < 0 or ss > 60:
-#         raise ValueError("Secondes Invalide")
-#     heure_alarme = f"{hh:02d}:{mm:02d}:{ss:02d}"
-# 
-#     while True:
-#         current_time = next(input_hour)
-#         print(f"{current_time} | Heure de l'alarme: {heure_alarme}", end="\r")
-#         if current_time == heure_alarme:
-#             print("\nL'alarme sonne")
-#             break
-#         time.sleep(1)
-# 
-# 
-time_tuple = (23, 59, 55)
+def alarme(input_tuple, input_hour):
+    hh = input_tuple[0]
+    mm = input_tuple[1]
+    ss = input_tuple[2]
+    if hh < 0 or hh > 24:
+        raise ValueError("Heure Invalide")
+    if mm < 0 or mm > 60:
+        raise ValueError("Minutes Invalide")
+    if ss < 0 or ss > 60:
+        raise ValueError("Secondes Invalide")
+    heure_alarme = f"{hh:02d}:{mm:02d}:{ss:02d}"
+
+    while True:
+        current_time = next(input_hour)
+        print(f"{current_time} | Heure de l'alarme: {heure_alarme}", end="\r")
+        if current_time == heure_alarme:
+            print("\nL'alarme sonne")
+            break
+        time.sleep(1)
+
+twelve = bool(format_choix())
+print(twelve)
+
+time_tuple = (10, 59, 55)
 display_heure = afficher_heure(time_tuple)
-# 
-# alarme_tuple = (0, 1, 2)
-# display_alarme = alarme(alarme_tuple, display_heure)
+
+alarme_tuple = (0, 1, 2)
+display_alarme = alarme(alarme_tuple, display_heure)
 
 while True:
     print(next(display_heure), end="\r")
